148404e594d212632d3089088ddc85de229b9e97
Camm66 <Cam.morales93@gmail.com>
2018-12-27 19:09:54 -0600

    feat: implemented sticky-nav

--- ResponsiveDesign/Portfolio/core/resources/js/script.js
+++ ResponsiveDesign/Portfolio/core/resources/js/script.js
@@ -1,5 +1,41 @@
  $(document).ready(function(){
      
+     
+/*-----------------Use waypoint to implement sticky nav---------------------*/
+    $('.portfolio-section').waypoint(function(direction){
+        if(direction == "down"){
+            $("#nav").addClass("sticky").appendTo("#js--sticky-nav");
+           }
+        else {
+            $("#nav").removeClass('sticky').appendTo("#js--home-nav");
+        }
+    }, {
+        offset: '60px;'
+    });
+     
+/*-------------------Cross current section in the nav-----------------------*/
+        $('.home-content').waypoint(function(direction){
+            $(".js--scroll-to-portfolio").removeClass("current-page-nav");
+       });
+     
+       $('.portfolio-section').waypoint(function(direction){
+           if(direction =="down"){
+               $(".js--scroll-to-portfolio").addClass("current-page-nav");
+           }
+       });
+     
+        $('.about-section').waypoint(function(direction){
+           if(direction =="down"){
+               $(".js--scroll-to-about").addClass("current-page-nav");
+                $(".js--scroll-to-portfolio").removeClass("current-page-nav");
+           }
+           else {
+               $(".js--scroll-to-portfolio").addClass("current-page-nav");
+               $(".js--scroll-to-about").removeClass("current-page-nav");
+           }
+       });
+     
+/*----------------------Scroll to selected section---------------------------*/
     $('.js--scroll-to-portfolio').click(function(){
         $('html, body').animate({scrollTop: $('.portfolio-section').offset().top}, 1000);
     });
@@ -8,5 +44,6 @@
         $('html, body').animate({scrollTop: $('.about-section').offset().top}, 1000);
     });
      
+     /*Circle text functions*/
      const circleType = new CircleType(document.getElementById('circle-text'));
 });
